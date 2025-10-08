090734de27e2f69356d7c9debd3a96a873c63d59
INFS502 <asthma.assistance@gmail.com>
2025-10-08 19:44:54 +1300

    file added

---
+++ main.py
@@ -0,0 +1,11 @@
+def to_upper(name):
+    return name.upper()
+
+def say_hello(name):
+    print(f'Hello, {name}')
+
+if _name__ == '_main_':
+    name = 'Tony'
+    say_hello(name)
+    up = to_upper(name)
+    print(up)
